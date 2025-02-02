046a4a0546a007e63d68b04ffffecf5accaf3daa
Joksa132 <nikolajoksimovic419@gmail.com>
2025-02-02 12:22:42 +0100

    add tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { AuthContext } from "@/components/AuthContext";
 import { Redirect, Tabs } from "expo-router";
 import { useContext } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
   const auth = useContext(AuthContext);
@@ -14,19 +15,61 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
+          tabBarLabelStyle: { fontWeight: "bold" },
           title: "Home",
+          tabBarLabelPosition: "below-icon",
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <Ionicons name="home" size={26} color="black" />
+            ) : (
+              <Ionicons name="home-outline" size={26} color="black" />
+            );
+          },
         }}
       />
       <Tabs.Screen
         name="scanner"
         options={{
-          title: "Barcode Scanner",
+          tabBarLabelStyle: { fontWeight: "bold" },
+          title: "Scanner",
+          tabBarLabelPosition: "below-icon",
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <Ionicons name="camera" size={26} color="black" />
+            ) : (
+              <Ionicons name="camera-outline" size={26} color="black" />
+            );
+          },
         }}
       />
       <Tabs.Screen
         name="add"
         options={{
+          tabBarLabelStyle: { fontWeight: "bold" },
           title: "Add food",
+          tabBarLabelPosition: "below-icon",
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <Ionicons name="add-circle" size={26} color="black" />
+            ) : (
+              <Ionicons name="add-circle-outline" size={26} color="black" />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          tabBarLabelStyle: { fontWeight: "bold" },
+          title: "Account",
+          tabBarLabelPosition: "below-icon",
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <Ionicons name="person" size={26} color="black" />
+            ) : (
+              <Ionicons name="person-outline" size={26} color="black" />
+            );
+          },
         }}
       />
     </Tabs>
