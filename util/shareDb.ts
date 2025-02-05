f7841bb2387fa4f0524b52458e40063e18a8de3c
Joksa132 <nikolajoksimovic419@gmail.com>
2025-02-05 21:36:23 +0100

    export function

--- util/shareDb.ts
+++ util/shareDb.ts
@@ -2,7 +2,7 @@ import * as Sharing from "expo-sharing";
 import * as FileSystem from "expo-file-system";
 import { Alert } from "react-native";
 
-async function handleShareDB() {
+export async function handleShareDB() {
   try {
     const dbFilePath = `${FileSystem.documentDirectory}SQLite/nutrition-track-db`; // Adjust the path if necessary
     const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
